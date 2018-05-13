5d6ecc8bc88e5e902423edf771c56e49b41a9e16
Sasha Sohrabi <shb.sohrabi@gmail.com>
2018-05-13 13:29:20 +0300

    navbar modification

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,5 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyDh9ldmxm2MMHMxoZY54cNdT9EdWOpal1s",
-    authDomain: "organic-shop-ac623.firebaseapp.com",
-    databaseURL: "https://organic-shop-ac623.firebaseio.com",
-    projectId: "organic-shop-ac623",
-    storageBucket: "",
-    messagingSenderId: "676738674640"
-  }
 };
 
